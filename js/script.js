e55c91c862000f20754f4ff8b50a4d49808fa4cf
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2019-12-16 17:35:12 +0100

    getElementById

--- js/script.js
+++ js/script.js
@@ -2,3 +2,5 @@
 
 /* ↓ Ho provato a giocare un po' con lo script per capire un po' come funzionano le concatenazioni ↓ */
 document.getElementsByClassName('main')[0].getElementsByClassName('main-content')[0].getElementsByClassName('main-content__soundtrack-box metal_lifting')[0].getElementsByClassName('title')[0].innerHTML='Lo script funziona <i class="far fa-thumbs-up"></i>';
+
+document.getElementById('stranger-title').innerHTML='Elemento gettato con successo <i class="fas fa-cat"></i>';
