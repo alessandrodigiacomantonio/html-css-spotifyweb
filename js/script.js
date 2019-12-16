9b8243e22177655e853380e98585e296bafe07cd
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2019-12-16 16:34:46 +0100

    Aggiunto script

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// alert('messaggio di allerta');
+
+/* ↓ Ho provato a giocare un po' con lo script per capire un po' come funzionano le concatenazioni ↓ */
+document.getElementsByClassName('main')[0].getElementsByClassName('main-content')[0].getElementsByClassName('main-content__soundtrack-box metal_lifting')[0].getElementsByClassName('title')[0].innerHTML='Lo script funziona <i class="far fa-thumbs-up"></i>';
